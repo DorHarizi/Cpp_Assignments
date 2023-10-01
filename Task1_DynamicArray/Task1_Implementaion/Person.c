4692f8a125f8db92b350a0689016f49812371daf
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-01 19:57:29 +0300

    Update Person.c

--- Task1_DynamicArray/Task1_Implementaion/Person.c
+++ Task1_DynamicArray/Task1_Implementaion/Person.c
@@ -1,61 +1,24 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include "AdptArray.h"
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "Person.h"
+// Define a structure for representing a person
+typedef struct PERSON_ {
+    char *first_name;
+    char *last_name;
+    int id;
+} person, *pperson;
 
-PElement copy_person(PElement original){
-	pperson org = (pperson)original;
-	pperson cp = (pperson)malloc(sizeof(person));
-	if(!cp) return NULL;
-	cp->first_name = (char*)malloc(strlen(org->first_name)+sizeof(char));
-	if(!cp->first_name) {
-		free(cp);
-		return NULL;
-	}
+// Function to copy a person
+PElement copy_person(PElement original);
 
-	cp->last_name = (char*)malloc(strlen(org->last_name)+sizeof(char));
-	if(!cp->last_name) {
-		free(cp->first_name);
-		free(cp);
-		return NULL;
-	}
-	
-	stpcpy(cp->first_name,org->first_name);
-	stpcpy(cp->last_name,org->last_name);
-	cp->id = org->id;
-	return cp;
-}
+// Function to delete a person
+void delete_person(PElement to_del);
 
-void delete_person(PElement to_del) {
-	pperson p = (pperson)to_del;
-	free(p->first_name);
-	free(p->last_name);
-	free(p);
-}
+// Function to print a person
+void print_person(PElement b_);
 
-void print_person(PElement p_){
-	pperson p = (pperson)p_;
-	printf("first name: %s last name: %s id: %d\n",p->first_name,p->last_name, p->id);
-}
+// Function to create a new person
+pperson create_person(char* first_name, char* last_name, int id);
 
-pperson creat_person(char* first_name , char* last_name, int id) {
-	pperson cp = (pperson)malloc(sizeof(person));
-	if(!cp) return NULL;
-	cp->first_name = (char*)malloc(strlen(first_name)+sizeof(char));
-	if(!cp->first_name) {
-		free(cp);
-		return NULL;
-	}
-	cp->last_name = (char*)malloc(strlen(last_name)+sizeof(char));
-	if(!cp->last_name) {
-		free(cp->first_name);
-		free(cp);
-		return NULL;
-	}
-	
-	stpcpy(cp->first_name,first_name);
-	stpcpy(cp->last_name,last_name);
-	cp->id = id;
-	return cp;
-}
+#endif
