5032d14e2b0102bbe35640f4c02f872186a74183
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-01 20:08:15 +0300

    Update book.h

--- Task1_DynamicArray/Task1_Implementaion/book.h
+++ Task1_DynamicArray/Task1_Implementaion/book.h
@@ -1,14 +1,18 @@
 #ifndef BOOK_H
 #define BOOK_H
+
 #include "AdptArray.h"
 
-typedef struct BOOK_{
-char *name;
-int serial_number;
+// Define a structure named 'book' with two members: 'name' and 'serial_number'
+typedef struct BOOK_ {
+    char *name;
+    int serial_number;
 } book, *pbook;
 
-PElement copy_book(PElement original);
-void delete_book(PElement to_del);
-void print_book(PElement b_);
-pbook creat_book(char* name , int serial_number);
+// Function declarations
+PElement copy_book(PElement original); // Function to copy a book
+void delete_book(PElement to_del);     // Function to delete a book
+void print_book(PElement b_);          // Function to print the details of a book
+pbook create_book(char *name, int serial_number); // Function to create a new book
+
 #endif
