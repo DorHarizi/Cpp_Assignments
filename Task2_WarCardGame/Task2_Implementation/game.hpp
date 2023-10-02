d89771d9f381d337f5e100daeabd68175c728ed1
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-02 20:55:01 +0300

    Update game.hpp

--- Task2_WarCardGame/Task2_Implementation/game.hpp
+++ Task2_WarCardGame/Task2_Implementation/game.hpp
@@ -5,11 +5,12 @@
 #include <vector>
 #include "player.hpp"
 #include "card.hpp"
-using namespace std;
-//using namespace ariel;
 
-namespace ariel{
-    class Game{
+using namespace std;
+
+namespace ariel {
+
+    class Game {
     private:
         Player p1;
         Player p2;
@@ -17,17 +18,30 @@ namespace ariel{
         string logGame;
         string status;
         vector<Card> package;
-        vector<string> gameProsess;
+        vector<string> gameProcess; 
         bool finish;
         string winner;
 
     public:
+        // Constructor for the Game class, initializes two players.
         Game(Player &player1, Player &player2);
+
+        // Simulate a single turn in the game.
         void playTurn();
+
+        // Print the details of the last turn.
         void printLastTurn();
+
+        // Simulate the entire game until completion.
         void playAll();
-        void printWiner();
+
+        // Print the winner of the game.
+        void printWinner(); // Fixed typo "printWiner" to "printWinner"
+
+        // Print the game log.
         void printLog();
+
+        // Print game statistics.
         void printStats();
     };
 }
