83c43bed542e77abb139f33ce3c8856a7409bfe6
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-02 20:44:51 +0300

    Update player.hpp

--- Task2_WarCardGame/Task2_Implementation/player.hpp
+++ Task2_WarCardGame/Task2_Implementation/player.hpp
@@ -4,33 +4,62 @@
 #include <vector>
 #include "card.hpp"
 using namespace std;
-//using namespace ariel;
 
-namespace ariel{
+namespace ariel {
 
-    class Player{
-        private:
-            string name;
-            vector<Card> packet;
-            bool isPlay;
-            int currentCard;
-            int num_of_cards_taken;
+    // Player class represents a player in the card game.
+    class Player {
+    private:
+        string name;              // The name of the player.
+        vector<Card> packet;     // The player's packet of cards.
+        bool isPlay;             // Flag indicating if the player is currently playing.
+        int currentCard;         // Index of the current card being played.
+        int num_of_cards_taken;  // Number of cards taken by the player.
 
-        public:
-            Player();
-            Player(string);
-            string getName(){return name;}
-            vector<Card> getPacket(){return packet;}
-            int cardesTaken();
-            int stacksize();
-            bool getIsPlay();
-            int getCurrentCard();
-            int getNumOfCard();
-            void setIsPlay(bool);
-            void setCurrentCard(int);
-            void setNumOfCards(int);
-            void setName(string);
-            void putCard(Card);
-            string to_string();
+    public:
+        // Default constructor for Player class.
+        Player();
+
+        // Parameterized constructor for Player class.
+        Player(string playerName);
+
+        // Getter function for player name.
+        string getName() { return name; }
+
+        // Getter function for player's packet of cards.
+        vector<Card> getPacket() { return packet; }
+
+        // Get the total number of cards taken by the player.
+        int cardesTaken();
+
+        // Get the number of cards in the player's packet.
+        int stacksize();
+
+        // Check if the player is currently playing.
+        bool getIsPlay();
+
+        // Get the index of the current card being played.
+        int getCurrentCard();
+
+        // Get the number of cards taken by the player.
+        int getNumOfCard();
+
+        // Set whether the player is currently playing.
+        void setIsPlay(bool isPlaying);
+
+        // Set the index of the current card being played.
+        void setCurrentCard(int current);
+
+        // Set the number of cards taken by the player.
+        void setNumOfCards(int numTaken);
+
+        // Set the name of the player.
+        void setName(string playerName);
+
+        // Add a card to the player's packet.
+        void putCard(Card newCard);
+
+        // Convert Player object to a string representation.
+        string to_string();
     };
 }
