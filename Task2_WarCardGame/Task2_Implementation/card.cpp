f0c6f15bb545dc9eecc0c50dd579839723a58e0f
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-02 20:41:12 +0300

    Update card.cpp

--- Task2_WarCardGame/Task2_Implementation/card.cpp
+++ Task2_WarCardGame/Task2_Implementation/card.cpp
@@ -5,42 +5,56 @@
 using namespace std;
 using namespace ariel;
 
-namespace ariel{
+namespace ariel {
 
+    // Default constructor for Card class.
     Card::Card() {
-                value = 0;
-                type = "";
-                name = ""; 
+        value = 0;
+        type = "";
+        name = "";
     }
-    
+
+    // Parameterized constructor for Card class.
     Card::Card(int newValue, string newType, string newName) {
         value = newValue;
         type = newType;
         name = newName;
     }
 
-    int Card::getValue(){return value;}
-    string Card::getType(){return type;}
-    string Card::getName(){return name;}
+    // Getter function for card value.
+    int Card::getValue() { return value; }
 
+    // Getter function for card type.
+    string Card::getType() { return type; }
+
+    // Getter function for card name.
+    string Card::getName() { return name; }
+
+    // Setter function for card value.
     void Card::setValue(int newValue) {
         value = newValue;
     }
 
+    // Setter function for card type.
     void Card::setType(string newType) {
         type = newType;
     }
 
+    // Setter function for card name.
     void Card::setName(string newName) {
         name = newName;
     }
 
-    bool Card::operator<(const Card &Crad2){return this->value < Crad2.value;}
-    bool Card::operator==(const Card &Crad2){return this->value == Crad2.value;}
+    // Less than operator for Card comparison.
+    bool Card::operator<(const Card& Card2) { return this->value < Card2.value; }
 
+    // Equality operator for Card comparison.
+    bool Card::operator==(const Card& Card2) { return this->value == Card2.value; }
+
+    // Convert Card object to a string representation.
     string Card::to_string() {
         string str = std::to_string(value);
-        // [Card: ("Queen", Diamonds, 12)]
-        return "[Card: ("+name+","+type+","+str+")]";
+        // Example: [Card: ("Queen", Diamonds, 12)]
+        return "[Card: (" + name + "," + type + "," + str + ")]";
     }
 }
